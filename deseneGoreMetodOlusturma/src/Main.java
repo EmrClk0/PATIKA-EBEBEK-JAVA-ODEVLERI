fe2c7198de2a4dfc3432da0c97a5815532c4e924
EmrClk0 <83767839+EmrClk0@users.noreply.github.com>
2022-11-06 16:09:15 +0300

    Add files via upload

---
+++ deseneGoreMetodOlusturma/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void pattern(int number, int temp, int signal){
+        System.out.print(temp+" ");
+        temp += (5*signal);
+
+        if(signal==1){
+            // sinyal 1 e döndüyse 1 le devam etsin
+            if(temp<=number)
+                pattern(number,temp,1);
+        }else{
+            // sinyal -1 se azaltam gerçekleşiyo
+            if(temp>0)
+                pattern(number,temp,-1);
+            else{
+                pattern(number,temp,1);
+            }
+
+        }
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        int number;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("N Sayısı : ");
+        number = scanner.nextInt();
+
+        pattern(number,number,-1);
+
+
+
+
+
+    }
+}
